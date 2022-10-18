8c3f8d627c69e3b4d8ac0f9e33bbabfca450c518
Matt <mhlindsey@outlook.com>
2022-10-18 13:03:08 -0700

    Created a RandomWord object and Scanner Object

---
+++ src/Hangman.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+
+        RandomWord Wordgen = new RandomWord();
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
